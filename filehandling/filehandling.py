b8794647b7640459a2edcf422db4f1fdb918259b
arminhadzalic <hadzalic@live.de>
2020-06-06 00:06:59 +0200

    Sending correct json file format

--- filehandling/filehandling.py
+++ filehandling/filehandling.py
@@ -2,12 +2,19 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+import json
+
 import sys
 sys.path.insert(0, '../flask/src/')
+
 import flask_client as fc
+import logutilities
 
 #show current working directory
 #pwd
+
+# Define the logger and the entry string (here "filehandling") within the log file
+logging = logutilities.logger.setLoggerName("filehandling")
 
 class EventHandler(FileSystemEventHandler):
     file_cache = {}
@@ -17,23 +24,43 @@ class EventHandler(FileSystemEventHandler):
         key = (seconds, event.src_path)
         print("Key: %s" %str(key))
         if key in self.file_cache:
-            print("Already registered event")
+            logging.debug("already registered event")
             return
         self.file_cache[key] = True
+        
         # Process the file
-        fc.sendPayload()
-        print(event)
+        file = event.src_path
+        processFile(event, file)
     
     #def on_any_event (if everything should be traced)
     #...
-    
+
+def processFile(event, file):
+    print("Trying to process file: %s" %file)
+    try:
+        with open(file, mode="r") as json_file:
+            content = json_file.read()
+            logging.info("Opened json-file with this content: %s" %content)
+            
+    except ValueError as err:
+        logging.error("Error while reading file: %s" %err)
+        return
+
+    #load rawdata and create string with correctly formated JSON - with double quotes
+    #{'a':1} --> {"a":1}
+    rawdata = json.loads(content)
+    data = json.dumps(rawdata)
+    logging.info("sending data from filewatcher to client")
+    fc.sendPayload(data)
+
+    print(event)
+
 
 def checkAndGetDir(params):
-    print("Number of arguments: ", len(sys.argv))
-    print("The arguments are: " , str(sys.argv))
     if len(sys.argv) >= 2:
         print("Path to be watched at is: %s" %str(sys.argv[1]))
         path = str(sys.argv[1])
+        #TODO check if path is accessible
         return path
     else:
         print("Path not provided! Closing application...")
@@ -41,6 +68,7 @@ def checkAndGetDir(params):
 
 
 def observeDir(path):
+    logging.info("Watching at: %s ..." % path)
     print("Watching at: %s ..." % path)
     event_handler = EventHandler()
     observer = Observer()
@@ -56,9 +84,8 @@ def observeDir(path):
 
 def main(params):
     path = checkAndGetDir(params)
-    watcher = observeDir(path)
+    observeDir(path)
         
-
 
 # call this *.py-file with:
 # python filehandling.py /path-to-your-folder/to-be-watched-for
